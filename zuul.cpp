a931e7d5133e4e055a59d569970772d76bfa0f50
cixay <alex.shan.young@gmail.com>
2017-11-29 11:18:46 -0800

    gogogo

--- zuul.cpp
+++ zuul.cpp
@@ -15,7 +15,7 @@ using namespace std;
 // need struct of items
 struct Item {
 	char name[30];
-	char description;
+	char description[100];
 	bool carried = true;
 	Room *location;
 };
@@ -38,7 +38,7 @@ int main() {
 	Room *NewBark = new Room("New Bark Town", "A town where the wind blows and tells of impending change.");
 	Room *Cherrygrove = new Room("Cherrygrove City", "A city where you can smell small flowers and a sea breeze.");
 	Room *Violet = new Room("Violet City", "This old village is still surrounded by trees and other scenery.");
-	Room *Azelea = new Room("Azalea Town", "A town where people and Pokemon live together in simple harmony.");
+	Room *Azalea = new Room("Azalea Town", "A town where people and Pokemon live together in simple harmony.");
 	Room *Goldenrod = new Room("Goldenrod City", "A developing city where people and Pokemon come and go as they like.");
 	Room *Ecruteak = new Room("Ecruteak City", "A city that even now bears the marks of its history.");
 	Room *Olivine = new Room("Olivine City", "A city where you can hear the melody of the sea.");
@@ -50,6 +50,7 @@ int main() {
 	Room *Park = new Room("National Park", "A spacious and beautiful park. It's conected to the Pokeathlon Dome.");
 	Room *Islands = new Room("Whirl Islands", "Four islands that are protected by whirlpools and are difficult to reach.");
 	Room *Safari = new Room("Safari Zone", "A place where you can experience wild thrills safely.");
+	Room *Mountain = new Room("Mt. Mortar", "A naturally large cavern that is incredibly spacious.");
 	Room *Lake = new Room("Lake of Rage", "A huge lake full of clear, blue water.");
 	Room *Path = new Room("Ice Path", "A cave that connects Route 44 to Blackthorn City.");
 	Room *Falls = new Room("Tohjo Falls", "The Tohjo waterfall that links the Kanto and Johto regions.");
@@ -62,11 +63,65 @@ int main() {
 	// N = 0 E = 1 S = 2 W = 3
 	NewBark -> setExit(1, Falls);
 	Falls -> setExit(3, NewBark);
-	city_1 -> setExit(3, place_1);
-	city_2 -> setExit(1, place_1);
-	city_2 -> setExit(0, city_3);
+	Falls -> setExit(0, Plateau);
+	Plateau -> setExit(2, Falls);
+	NewBark -> setExit(3, Cave);
+	Cave -> setExit(1, NewBark);
+	Cave -> setExit(3, Cherrygrove);
+	Cherrygrove -> setExit(1, Cave);
+	Cave -> setExit(0, Blackthorn);
+	Blackthorn -> setExit(2, Cave);
+	Cherrygrove -> setExit(3, Violet);
+	Violet -> setExit(1, Cherrygrove);
+	Violet -> setExit(2, Azelea);
+	Azalea -> setExit(0, Violet);
+	Azalea -> setExit(3, Forest);
+	Forest -> setExit(1, Azalea);
+	Forest -> setExit(0, Goldenrod);
+	Goldenrod -> setExit(2, Forest);
+	Goldenrod -> setExit(0, Park);
+	Park -> setExit(2, Goldenrod);
+	Park -> setExit(0, Ecruteak);
+	Park -> setExit(1, Violet);
+	Violet -> setExit(3, Park);
+	Ecruteak -> setExit(2, Park);
+	Ecruteak -> setExit(3, Olivine);
+	Olivine -> setExit(1, Ecruteak);
+	Olivine -> setExit(2, Islands);
+	Islands -> setExit(0, Olivine);
+	Islands -> setExit(3, Cianwood);
+	Cianwood -> setExit(1, Islands);
+	Cianwood -> setExit(3, Safari);
+	Safari -> setExit(1, Cianwood);
+	Ecruteak -> setExit(1, Mountain);
+	Mountain -> setExit(3, Ecruteak);
+	Mountain -> setExit(1, Mahogany);
+	Mahogany -> setExit(3, Mountain);
+	Mahogany -> setExit(0, Lake);
+	Lake -> setExit(2, Mahogany);
+	Mahogany -> setExit(3, Path);
+	Path -> setExit(1, Mahogany);
+	Path -> setExit(2, Blackthorn);
+	Blackthorn -> setExit(0, Path);
 
 	// next I need to add all my items
+	Item zephyr;
+	zephyr.name = "Zephyr Badge";
+	zephyr.descpription = "You have defeated gym leader Falkner";
+	zephyr.carried = false;
+	Item hive;
+	hive.name = "Hive Badge";
+	hive.descpription = "You have defeated gym leader Bugsy";
+	hive.carried = false;
+	Item plain;
+	Item fog;
+	Item storm;
+	Item mineral;
+	Item glacier;
+	Item rising;
+	
+
+
 
 	// need to play the game with win conditions
 
